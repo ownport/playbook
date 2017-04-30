f6bde8124a55ef44bbae4dd78a864eb8426f7c54
ownport <ownport@gmail.com>
2017-04-30 20:34:34 +0300

    [+] loop processing + tests

---
+++ test/actions/loops/test_foreach.py
@@ -0,0 +1,10 @@
+from playbook.actions.loops.foreach import Action
+
+
+def test_actions_loops_foreach_create():
+    foreach = Action()
+    assert isinstance(foreach, Action)
+
+def test_actions_loops_foreach_run():
+    foreach = Action(*[1,2,3])
+    assert [i[u'data'] for i in foreach.run()] == [1,2,3]
--- test/test_action_command.py
+++ test/test_action_command.py
@@ -3,6 +3,7 @@ import pytest
 from playbook.actions import ActionExecutor
 from playbook.errors import UnknownAction
 
+
 def test_action_command_instance():
     assert isinstance(ActionExecutor('shell'), ActionExecutor)
 
@@ -11,13 +12,14 @@ def test_action_command_apply_simple_cmd():
     args = ['echo', 'Test playbook']
     action = ActionExecutor('shell', *args)
     assert action.name == 'playbook.actions.basic.shell'
-    assert action.apply() == {
-        'status': 'SUCCESS',
-        'exitcode': 0,
-        'stdout': 'Test playbook\n',
-        'stderr': '',
-        'msg': ''
-    }
+
+    for r in action.apply({}):
+        assert r.get(u'status') == u'SUCCESS'
+        assert r.get(u'exitcode') == 0
+        assert r.get(u'stdout') == u'Test playbook\n'
+        assert r.get(u'stderr') == u''
+        assert r.get(u'msg') == u''
+
 
 def test_action_command_apply_simple_cmd_with_error():
     args = ['echo', 'Test playbook']
