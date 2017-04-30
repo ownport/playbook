ee5eb01cebc1ce3b7f4e6f88d19d580e8163e557
ownport <ownport@gmail.com>
2017-04-30 23:08:26 +0300

    [*] fixed errorcode

--- src/playbook/actions/basic/shell.py
+++ src/playbook/actions/basic/shell.py
@@ -15,7 +15,7 @@ class Action(BaseAction):
         try:
             exitcode, stdout, stderr = local.get(self._args[0]).run(self._args[1:])
         except ProcessExecutionError as err:
-            rc = err.retcode
+            exitcode = err.retcode
             stdout = err.stdout
             stderr = err.stderr
             msg = err.stderr
