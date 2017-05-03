c65854d3ed9ac29bef639739ac99859fd491f4be
ownport <ownport@gmail.com>
2017-05-03 21:08:54 +0300

    [-] remove prev implemenation

--- test/actions/basic/test_shell.py
+++
@@ -1,8 +0,0 @@
-from __future__ import (absolute_import, division, print_function)
-
-from playbook.actions.basic.shell import Action
-
-def test_action_basic_shell():
-
-    assert Action()
-    # TODO add more test cases
